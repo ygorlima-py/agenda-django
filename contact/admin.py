2f61c4bfad6fde593a116a5df3af75086e82e968
Ygor Lima <ygor.limarsx@gmail.com>
2025-10-23 09:45:13 -0300

    Criando foreign key e a tabela Category

--- contact/admin.py
+++ contact/admin.py
@@ -13,3 +13,8 @@ class ContactAdmin(admin.ModelAdmin):
     list_max_show_all = 50
     list_editable = 'first_name', 'last_name',
     list_display_links = 'id', 'phone',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -11,7 +17,14 @@ class Contact(models.Model):
     description = models.TextField()
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(
+                                Category, 
+                                on_delete=models.SET_NULL,
+                                blank=True,
+                                null=True,
+                                )
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
     
+
