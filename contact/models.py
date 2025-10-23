bf4c243538c36692ef9d4f1b3ffffe45590a20f9
Ygor Lima <ygor.limarsx@gmail.com>
2025-10-23 09:53:54 -0300

    verbose_name e verbose_name_plural

--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils import timezone
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
