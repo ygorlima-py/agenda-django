0ca9ac18cb8a503737db3a691af934d2331f50f3
Ygor Lima <ygor.limarsx@gmail.com>
2025-10-24 09:37:20 -0300

    Criação do de nav para o menu e novos templates para single contact

--- contact/urls.py
+++ contact/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from contact.views import index
+from contact.views import index, contact
 
 app_name = 'contact'
 
 urlpatterns = [
+    
+    path('<int:contact_id>/',contact, name='contact'),
     path('',index, name='index'),
+
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render
+from django.http import Http404
 from contact.models import Contact
 
 # Create your views here.
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
 
-    print(contacts.query)
+
     context = dict(
         contacts=contacts,
     )
     return render(
         request,
         'contact/index.html',
+        context,
+    )
+
+
+
+def contact(request, contact_id):
+    single_contact = Contact.objects.filter(pk=contact_id, show=True).first()
+
+    if single_contact is None:
+        raise Http404()
+    
+    context = {'contact': single_contact}
+
+    return render(
+        request,
+        'contact/contact.html',
         context,
     )
