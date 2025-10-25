862daaf82e6d7833493beaf774481a027351e1f5
Ygor Lima <ygor.limarsx@gmail.com>
2025-10-25 08:00:36 -0300

    Usando Padrão comum de URLs CRUD

--- contact/urls.py
+++ contact/urls.py
@@ -4,8 +4,9 @@ from contact.views import index, contact, search
 app_name = 'contact'
 
 urlpatterns = [
+    # contact(CRUD)
+    path('contact/<int:contact_id>/detail', contact, name='contact'),
     
-    path('<int:contact_id>/', contact, name='contact'),
     path('search/', search, name='search'),
     path('', index, name='index'),
 
