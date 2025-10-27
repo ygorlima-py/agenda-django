ddcc37f6fd5c04b389b53b012b650b37c1519989
Ygor Lima <ygor.limarsx@gmail.com>
2025-10-27 05:14:04 -0300

    Configurando widgts do formulário

--- contact/forms.py
+++ contact/forms.py
@@ -4,6 +4,27 @@ from contact.models import Contact
 
 
 class ContactForm(forms.ModelForm):
+    
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+            'class': 'class-a',
+            'placeholder': 'Digite seu nome'
+            }
+        ),
+        label='Primeiro nome',
+        help_text='Texto de ajuda para o meu usuário'
+    )
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # self.fields['first_name'].widget.attrs.update({
+        #     'class': 'class-a',
+        #     'placeholder': 'Digite seu nome'
+        # })
+
     class Meta:
         model = Contact
         fields = (
@@ -11,6 +32,19 @@ class ContactForm(forms.ModelForm):
             'last_name',
             'phone',
         )
+
+        # Widgetes são tipos pra definir o tipo de campo que vai 
+        # ser reenderizado, pode ser do tipo:
+        # PasswordInput(), TextArea(), TextInput
+
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'class': 'class-a',
+        #             'placeholder': 'Digite seu nome'
+        #         }
+        #     ) 
+        # }
 
     def clean(self):
         cleaned_data = self.cleaned_data
