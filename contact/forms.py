f34c425793a530e10d75d4cfc85fb2c93114bcbf
Ygor Lima <ygor.limarsx@gmail.com>
2025-10-27 13:17:57 -0300

    Validando campos first_name, last_name e email do Usuário

--- contact/forms.py
+++ contact/forms.py
@@ -2,6 +2,7 @@ from django.core.exceptions import ValidationError
 from django import forms
 from contact.models import Contact
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ContactForm(forms.ModelForm):
     picture = forms.ImageField(
@@ -41,4 +42,39 @@ class ContactForm(forms.ModelForm):
         return super().clean()
 
 class RegisterForm(UserCreationForm):
-    ...
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+
+    email = forms.EmailField()
+
+    
+    
+
+    class Meta:
+        model = User # Usamos o Objeto User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+        )
+
+
+    # O método clen_email faz a validação usando atributo herdado da classe
+    # UserCreationForm para pegar o valor do email e então validando usando
+    # User.objects.filter(email=email).exists():
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Email já cadastrado', code='invalid')
+            )
+
+        return email
