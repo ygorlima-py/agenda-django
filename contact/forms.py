6f250be763495a92ed9dcb13f4b6ffc905c7bb5a
Ygor Lima <ygor.limarsx@gmail.com>
2025-10-27 05:36:54 -0300

    Validando os campos com clean, clean field para apenas um campo, e ValidationError

--- contact/forms.py
+++ contact/forms.py
@@ -48,13 +48,30 @@ class ContactForm(forms.ModelForm):
 
     def clean(self):
         cleaned_data = self.cleaned_data
-        
-        self.add_error(
-            None,
+        first_name = cleaned_data.get('first_name')
+        lastst_name = cleaned_data.get('last_name')
+
+        if first_name == lastst_name:
+            msg = ValidationError(
+                    'Primeiro nome não pode ser igual ao segundo',
+                    code='invalid',
+                )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+                
+            
+
+        return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error(
+            'first_name',
             ValidationError(
                 'Mensagem de erro',
                 code='invalid',
             )
         )
-
-        return super().clean()
+        return first_name
