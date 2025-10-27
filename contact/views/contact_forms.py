621a52ad42d95108d7bc0f7923d90f32226c3b9c
Ygor Lima <ygor.limarsx@gmail.com>
2025-10-27 06:54:06 -0300

    salvando e validando os dados do formulário

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from contact.forms import ContactForm
 
 def create(request):
     if request.method == 'POST':
+        form = ContactForm(request.POST)
         context = dict(
-            form=ContactForm(request.POST),
+            form=form,
         )
+
+        if form.is_valid():
+            print('Formulario Válido')
+            form.save()
+            return redirect('contact:create')
 
         return render(
         request,
